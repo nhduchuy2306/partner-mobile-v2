e9a5844f75ecfbc8fa6b47a9df51424fd15297fa
nhduchuy2306 <nguyenhuy1323614@gmail.com>
2023-06-24 09:59:41 +0700

    update notification foreground

--- lib/provider/firebase_message_provider.dart
+++ lib/provider/firebase_message_provider.dart
@@ -1,42 +1,96 @@
+import 'dart:math';
+
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 class MyFirebaseMessagingProvider {
   Future<void> initialize() async {
     await FirebaseMessaging.instance.requestPermission(
-      alert: true,
-      announcement: false,
-      badge: true,
-      carPlay: false,
-      criticalAlert: false,
-      provisional: false,
-      sound: true
-    );
+        alert: true,
+        announcement: false,
+        badge: true,
+        carPlay: false,
+        criticalAlert: false,
+        provisional: false,
+        sound: true);
 
-    await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
-      alert: true,
-      badge: true,
-      sound: true
-    );
+    await FirebaseMessaging.instance
+        .setForegroundNotificationPresentationOptions(
+            alert: true, badge: true, sound: true);
 
     FirebaseMessaging.onMessage.listen((RemoteMessage message) async {
-      print("onMessage: ${message.notification?.title}");
-      print("onMessage: ${message.notification}");
+      RemoteNotification notification = message.notification!;
+      AndroidNotification android = message.notification!.android!;
+      if (notification != null && android != null) {
+        sendNotification(title: notification.title, body: notification.body);
+      }
     });
 
     FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) async {
-      print("onResume: ${message.notification?.title}");
-      print("onResume: ${message.notification?.body}");
+      RemoteNotification notification = message.notification!;
+      AndroidNotification android = message.notification!.android!;
+      if (notification != null && android != null) {
+        sendNotification(title: notification.title, body: notification.body);
+      }
     });
 
     FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
   }
 
-  static Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  static Future<void> _firebaseMessagingBackgroundHandler(
+      RemoteMessage message) async {
     print("onBackgroundMessage: $message");
   }
 
   Future<String?> getFCMToken() async {
     String? token = await FirebaseMessaging.instance.getToken();
     return token;
+  }
+
+  void sendNotification({String? title, String? body}) async {
+    FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+        FlutterLocalNotificationsPlugin();
+
+    const AndroidInitializationSettings initializationSettingsAndroid =
+        AndroidInitializationSettings('@mipmap/ic_launcher');
+
+    const InitializationSettings initializationSettings =
+        InitializationSettings(android: initializationSettingsAndroid);
+    await flutterLocalNotificationsPlugin.initialize(
+      initializationSettings,
+    );
+
+    const AndroidNotificationChannel channel = AndroidNotificationChannel(
+        'high_channel', 'High Importance Notification',
+        description: "This channel is for important notification",
+        importance: Importance.max);
+
+    AndroidNotificationDetails androidNotificationDetails =
+        AndroidNotificationDetails(
+      channel.id,
+      channel.name,
+      channelDescription: channel.description,
+      importance: Importance.max,
+      priority: Priority.high,
+      showWhen: false,
+      playSound: true,
+      enableVibration: true,
+      enableLights: true,
+      color: Colors.blue,
+      ledColor: Colors.blue,
+      ledOnMs: 1000,
+      ledOffMs: 500,
+    );
+
+    flutterLocalNotificationsPlugin.show(
+      Random().nextInt(100),
+      title,
+      body,
+      NotificationDetails(
+        android: androidNotificationDetails,
+      ),
+      payload: 'high importance',
+    );
   }
 }
